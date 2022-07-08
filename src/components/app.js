e7fde14c87529770887fb5821424b33032489160
Braxton Newhall <25bnewhall@go.dsdmail.net>
2022-07-08 10:18:06 -0600

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Braxton Newhall Portfolio</h1>
-
         {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        <PortfolioContainer />
       </div>
     );
   }
